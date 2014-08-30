025caf73e922f5fe206199e6ca07f4e6d8f09e95
Ovi Farcas <farcaso@gmail.com>
2014-08-30 13:02:20 +0200

    Fix drustall script

--- config.sh
+++ config.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-# Install folder
-docroot = ''
-# Main destination
-drupal_dir="/var/www"
-# Specific destination. Leave blank if desired to install in root
-drupal_subdir = 'drupal'
+# How to read the input from command line variable=$1 or $2, $3 etc. 
+# $0 is considered the command itself
+#  if [ $# -ne 2 ]; then
+#   echo "Usage: $0 [arg1] [arg2]"
+#   exit
+# fi
 
-site_name = 'Demo Drupal'
+# Install folder
+docroot=''
+# Main destination
+drupal_dir='/var/www'
+# Specific destination. Leave blank if desired to install in root
+drupal_subdir='drupal'
+
+site_name='Demo Drupal'
 
 # Database
-db_user = 'root'
-db_pass = ''
-db_host = 'localhost'
-db_port = '3306'
+db_user='root'
+db_pass='dev'
+db_host='localhost'
+db_port='3306'
 
-# Poweruser uid = 1
-user_name = 'admin'
-user_pass = 'pass'
-user_mail = 'admin@example.com'
+db_name='drupal'
+
+# Poweruser uid=1
+user_name='admin'
+user_pass='pass'
+user_mail='admin@example.com'
+
+# Make file
+make_file='drupal-org.make'
 
